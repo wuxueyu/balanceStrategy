a40785a0b14953e77622c98a9961be6a8d716070
wuxueyu <wuxueyu@wuxueyudeMacBook-Pro-2.local>
2018-09-27 14:20:14 +0800

    add banlance 算法

---
+++ index.js
@@ -0,0 +1,8 @@
+
+module.exports = {
+
+    WeightedRoundRobin: require("lib/weightedRoundRobin"),
+    Random:require("lib/random"),
+    RandomWeight:require("lib/random_weight"),
+    RoundWeight:require("lib/round_robin")
+}
